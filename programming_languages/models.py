7af2f006145bea1acd550f80a7ae80805ef4079f
Milin Mestry <milinmestry@gmail.com>
2017-07-29 00:32:25 +0530

    New model created for Programming Languages master table.

--- programming_languages/models.py
+++ programming_languages/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class ProgrammingLanguage(models.Model):
+    programming_lang = models.CharField(max_length=20)
+    description = models.TextField(max_length=300, blank=True, null=True)
+    icno_url = models.URLField(max_length=200, blank=True, null=True)
+    is_active = models.BooleanField(blank=True)
+    added_by = models.CharField(max_length=100, blank=True)
+    added_on = models.DateTimeField(blank=True)
+    updated_by = models.CharField(max_length=100, blank=True, null=True)
+    updated_on = models.DateTimeField(blank=True, null=True)
+    deactivated_by = models.CharField(max_length=100, blank=True, null=True)
+    deactivated_on = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.programming_lang
