61e49a9230c4f3dd0ebec2d7fad2c80af20894f3
Milin Mestry <milinmestry@gmail.com>
2017-08-08 00:28:41 +0530

    Facing error: details.html template does not exist for Programming Language.

--- programming_languages/urls.py
+++ programming_languages/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'programming_languages'
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='details'),
 ]
--- programming_languages/views.py
+++ programming_languages/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect #
 from django.urls import reverse
+from django.views import generic
 
-# from .models import Choice, Question
+
+from .models import ProgrammingLanguage
 
 # Create your views here.
 
@@ -11,3 +13,14 @@ def index(request):
     # context = {'latest_question_list': latest_question_list}
     # return render(request, 'polls/index.html', context)
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+class DetailView(generic.DetailView):
+    model = ProgrammingLanguage
+    template_name = 'programming_languages/details.html'
+
+    # def get_queryset(self):
+    #     """
+    #     Exclude any questions thar aren't published yet.
+    #     """
+    #     return Question.objects.filter(pub_date__lte=timezone.now())
